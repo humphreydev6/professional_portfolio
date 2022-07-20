dfbef30b4e512720512c4b7a438e39cb60266281
Humphrey Ikhalea <84229187+humphreydev6@users.noreply.github.com>
2022-07-20 16:59:37 +0100

    first commit

---
+++ backend_sanity/schemas/brands.js
@@ -0,0 +1,19 @@
+export default {
+    name: 'brands',
+    title: 'Brands',
+    type: 'document',
+    fields: [{
+            name: 'imgUrl',
+            title: 'ImgUrl',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        },
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+    ]
+};
